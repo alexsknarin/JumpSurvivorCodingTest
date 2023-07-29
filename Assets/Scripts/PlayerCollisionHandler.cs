9e3bddf916c7ccee695b43bf1db8637675b27e3b
alexsknarin <alexandr.sknarin@gmail.com>
2023-07-29 11:32:07 -0700

    added flashing on collision

--- Assets/Scripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    public delegate void OnEnemyCollideAction();
+    public static event OnEnemyCollideAction OnEnemyCollided;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("Hit");
+            if (OnEnemyCollided != null)
+            {
+                OnEnemyCollided();    
+            }
         }
     }
 }
--- Assets/Scripts/PlayerFlashingHit.cs
+++ Assets/Scripts/PlayerFlashingHit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,49 @@ public class PlayerFlashingHit : MonoBehaviour
 {
     [SerializeField] private Color _flashColor;
     [SerializeField] private float _flashFreq;
+    [SerializeField] private float _flashDuration;
     private float _flashValue;
     private Material _material;
-    
+    private bool _isFlashing;
+    private float _prevTime;
+
+    private void OnEnable()
+    {
+        PlayerCollisionHandler.OnEnemyCollided += StartFlashing;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         _material = GetComponent<MeshRenderer>().material;
+        _isFlashing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_isFlashing)
+        {
+            Flashing();    
+        }
+    }
+
+    private void Flashing()
+    {
         _flashValue = (Mathf.Sin(Time.time * _flashFreq) + 1f) / 2f; 
         _material.color = Color.Lerp(Color.white, _flashColor, _flashValue);
+        if (Time.time - _prevTime > _flashDuration)
+        {
+            _isFlashing = false;
+            _material.color = Color.white;
+        }
+        
+    }
+
+    private void StartFlashing()
+    {
+        _isFlashing = true;
+        _prevTime = Time.time;
     }
 }
