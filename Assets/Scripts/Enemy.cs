84e39cc02486a348e710dd5afd4cd27a20277b8c
alexsknarin <alexandr.sknarin@gmail.com>
2023-07-29 21:01:03 -0700

    basic enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _direction;
-    
-    
+    private Vector3 _spawnPos = Vector3.zero;
+    private float _direction;
+   
     void Start()
     {
-        
+        _spawnPos.x = 20f;
+        SpawnSetup(1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();        
+    }
+
+    public void SpawnSetup(float dir)
+    {
+        _spawnPos.x *= -dir;
+        _spawnPos.y = 0.5f;
+        transform.position = _spawnPos;
+        _direction = dir;
+    }
+
+    private void Move()
+    {
         transform.Translate(Vector3.right * Time.deltaTime *_speed * _direction);
     }
+    
 }
