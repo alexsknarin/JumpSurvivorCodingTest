8ca34be2abb83c327e2e1feca2afd52f8b34e75e
alexsknarin <alexandr.sknarin@gmail.com>
2023-08-07 13:41:39 -0700

    added player Data and time to gameover text. Escape restarts the game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,23 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
     // Player
     [SerializeField] private IntVariable _playerHealth;
-    
+    [SerializeField] private FloatVariable _gameTime;
     // Enemies
     [SerializeField] private SpawnManager _spawnManager;
     
     // Global Game UI
     [SerializeField] private GameObject _inGameUI;
     [SerializeField] private GameObject _gameOverUI;
+    [SerializeField] private TextMeshProUGUI _gameOverStats;
+    [SerializeField] private StringVariable _currentUserName;
  
     // Pause Handling
     public static List<IPausable> Pausables = new List<IPausable>();
+
+    private bool _isGameOver = false;
 
     private void OnEnable()
     {
@@ -56,6 +62,11 @@ public class Game : MonoBehaviour
         {
             UnPauseGame();
         }
+        if (Input.GetKeyDown(KeyCode.Escape) && _isGameOver)
+        {
+            SceneManager.LoadScene(0);
+        }
+        
     }
 
     private void Start()
@@ -68,7 +79,10 @@ public class Game : MonoBehaviour
         if (_playerHealth.Value == 0)
         {
             _gameOverUI.SetActive(true);
+            _inGameUI.SetActive(false);
+            _gameOverStats.text = "Player Name: " + _currentUserName.Value + "\n" + "Time: " + ((int)_gameTime.Value).ToString() + " seconds";
             PauseGame();
+            _isGameOver = true;
         }
     }
 }
