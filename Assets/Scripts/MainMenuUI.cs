faab8b884599efa3e1e80298f67d7d6bf5e48cf9
alexsknarin <alexandr.sknarin@gmail.com>
2023-08-08 01:03:10 -0700

    Main menu script is ready
    user name and difficulty level is saved into SO

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField _userNameInput;
+    [SerializeField] private TMP_Dropdown _difficutlyLevelDropdown;
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _hightScoreButton;
+    [SerializeField] private Button _exitGameButton;
+    [SerializeField] private IntVariable _difficultyLevelVariable;
+    [SerializeField] private StringVariable _userNameVariable;
+
+    private void Start()
     {
-        
+        _startGameButton.onClick.AddListener(StartGame);
+        _exitGameButton.onClick.AddListener(ExitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartGame()
     {
-        
+        string userName = _userNameInput.text;
+        if (userName == "")
+        {
+            userName = "noname";
+        }
+        _userNameVariable.Value = userName;
+        _difficultyLevelVariable.Value = _difficutlyLevelDropdown.value;
+        SceneManager.LoadScene(0);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+        _difficultyLevelVariable.Value = 0;
+        _userNameVariable.Value = "noname";
     }
 }
