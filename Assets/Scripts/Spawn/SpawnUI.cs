7470ab35355fea828e7db5f34de50477f77a66d9
alexsknarin <alexandr.sknarin@gmail.com>
2023-12-28 21:18:17 -0800

    easy mode assembled

--- Assets/Scripts/Spawn/SpawnUI.cs
+++ Assets/Scripts/Spawn/SpawnUI.cs
@@ -7,10 +7,18 @@ using UnityEngine;
 public class SpawnUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private List<Color> _randomColors;
+    private int _colorIndex = 0;
 
     private void ChangeText(string newText)
     {
         _text.text = newText;
+        _text.color = _randomColors[_colorIndex];
+        _colorIndex++;
+        if (_colorIndex > _randomColors.Count - 1)
+        {
+            _colorIndex = 0;
+        }
     }
 
     private void OnEnable()
