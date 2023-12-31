c370da6103046c857b9941f2369cb01fd4a87ae9
alexsknarin <alexandr.sknarin@gmail.com>
2023-12-30 19:05:45 -0800

    Kangaroo: out of jump animation on collision with ground

---
+++ Assets/Scripts/EnemyGroundCollider.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyGroundCollider : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    private IGroundCollidable _enemyCollidable;
+
+    private void Awake()
+    {
+        _enemyCollidable = _enemy.GetComponent<EnemyKangarooViewHandler>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            _enemyCollidable.CollidedWIthGround();
+        }
+    }
+}
--- Assets/Scripts/EnemyKangarooViewHandler.cs
+++ Assets/Scripts/EnemyKangarooViewHandler.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(EnemyKangaroo))]
-public class EnemyKangarooViewHandler : MonoBehaviour
+public class EnemyKangarooViewHandler : MonoBehaviour, IGroundCollidable
 {
     [SerializeField] private GameObject _kangarooVeiewBase;
     private Vector3 _xFlip = Vector3.one;
     [SerializeField] private Animator _kangarooAnimator;
     [SerializeField] private EnemyKangaroo _enemyKangaroo;
+    private static readonly int JumpPhase = Animator.StringToHash("jumpPhase");
+    private static readonly int OnGround = Animator.StringToHash("onGround");
 
     private void Update()
     {
@@ -25,6 +27,16 @@ public class EnemyKangarooViewHandler : MonoBehaviour
         _kangarooVeiewBase.transform.localScale = _xFlip;
         
         // Anim control
-        _kangarooAnimator.SetFloat("jumpPhase", _enemyKangaroo.JumpPhase);
+        _kangarooAnimator.SetFloat(JumpPhase, _enemyKangaroo.JumpPhase);
+
+        if (_enemyKangaroo.JumpPhase > 0.1f)
+        {
+            _kangarooAnimator.SetBool(OnGround, false);
+        }
+    }
+
+    public void CollidedWIthGround()
+    {
+        _kangarooAnimator.SetBool(OnGround, true);
     }
 }
