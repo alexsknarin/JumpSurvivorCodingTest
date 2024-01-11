ccc3e3b5035dbcd07041670dddf0af3b452e86e1
alexsknarin <alexandr.sknarin@gmail.com>
2024-01-10 21:38:24 -0800

    add heal for every 25th bonus point. UI animation ready

--- Assets/Scripts/UI/HealthHeartAnimation.cs
+++ Assets/Scripts/UI/HealthHeartAnimation.cs
@@ -1,4 +1,7 @@
-using System;
+/* Heal animation uses transform of the bonus text UI
+ * it's position supplied as an argument for DoHeal
+ * 
+ */
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +12,9 @@ enum HeartStates
      HealAnimState,
      NearDeathAnimState
 }
-
+/// <summary>
+/// All animations for health heart
+/// </summary>
 public class HealthHeartAnimation : MonoBehaviour
 {
      [SerializeField] private Image _heartImage;
@@ -18,34 +23,60 @@ public class HealthHeartAnimation : MonoBehaviour
      [SerializeField] private Color _nearDeathColor;
      [SerializeField] private AnimationCurve _damageAnimCurve;
 
-     private float _damageAnimationPhase = 0f;
+     private float _animationPhase = 0f;
      [SerializeField] private float _damageAnimationSpeed;
      [SerializeField] private float _nearDeathFlashFrequency;
      private float _nearDeathPulse;
-     
-
+     [SerializeField] private float _healAnimationSpeed;
+     [SerializeField] private AnimationCurve _healScaleAnimCurve;
+     [SerializeField] private AnimationCurve _healMoveAnimCurve;
+     private Vector3 _parentPosition;
+     private Vector3 _instancePersistentPosition;
+     private Vector3 _healStartPosition;
      private HeartStates _heartState = HeartStates.NormalState;
 
+     public void SaveInitialState(Vector3 currentPos, Vector3 parentPos)
+     {
+          _instancePersistentPosition = currentPos;
+          _parentPosition = parentPos;
+     }
+     
      public void DoDamage()
      {
-          Debug.Log("Damaged");
           _heartState = HeartStates.DamageAnimState;
           _heartImage.color = _nearDeathColor;
+          _animationPhase = 0f;
      }
-
+     
      private void DamageAnimationState()
      {
-          transform.localScale = Vector3.one * _damageAnimCurve.Evaluate(_damageAnimationPhase);
-          _damageAnimationPhase += _damageAnimationSpeed*Time.deltaTime;
-          if (_damageAnimationPhase > 1)
+          transform.localScale = Vector3.one * _damageAnimCurve.Evaluate(_animationPhase);
+          _animationPhase += _damageAnimationSpeed*Time.deltaTime;
+          if (_animationPhase > 1)
           {
-               this.gameObject.SetActive(false);
+               gameObject.SetActive(false);
           }
      }
 
-     public void DoHeal()
+     public void DoHeal(Vector3 startPos)
      {
-          Debug.Log("Healed");
+          gameObject.SetActive(true);
+          _healStartPosition = startPos - _parentPosition;
+          _animationPhase = 0;
+          _heartState = HeartStates.HealAnimState;
+     }
+     
+     private void HealAnimationState()
+     {
+          transform.localScale = Vector3.one * _healScaleAnimCurve.Evaluate(_animationPhase);
+          transform.localPosition = Vector3.Lerp(_healStartPosition, _instancePersistentPosition, _healMoveAnimCurve.Evaluate(_animationPhase));
+          _animationPhase += _healAnimationSpeed*Time.deltaTime;
+          if (_animationPhase > 1)
+          {
+               _heartState = HeartStates.NormalState;
+               transform.localPosition = _instancePersistentPosition;
+               _heartImage.color = _baseColor;    
+          }
      }
 
      public void DoNearDeath()
@@ -69,6 +100,10 @@ public class HealthHeartAnimation : MonoBehaviour
           else if(_heartState == HeartStates.NearDeathAnimState)
           {
                NearDeathState();
+          }
+          else if(_heartState == HeartStates.HealAnimState)
+          {
+               HealAnimationState();
           }
      }
 }
--- Assets/Scripts/UI/PlayerHealthUIView.cs
+++ Assets/Scripts/UI/PlayerHealthUIView.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// Responsible for lifebar and health icon management. Link to _bonusTextTransform is for hearts heal animation;
+/// </summary>
 public class PlayerHealthUIView : MonoBehaviour
 {
     [SerializeField] private HealthHeartAnimation _healthHeartPrefab;
     [SerializeField] private IntVariable _playerHealth;
     [SerializeField] private IntVariable _maxHealth;
     [SerializeField] private float _heartsDistance;
+    [SerializeField] private Transform _bonusTextTransform;
     private Vector3 _newPosition = Vector3.zero;
     private List<HealthHeartAnimation> _healthHearts = new List<HealthHeartAnimation>();
     private bool _nearDeath = false;
@@ -19,12 +20,14 @@ public class PlayerHealthUIView : MonoBehaviour
     {
         PlayerHealth.OnPlayerDamaged += RecieveDamage;
         PlayerHealth.OnPlayerHealthSetUp += Initialize;
+        PlayerHealth.OnLifeIncreased += HealDamage;
     }
 
     private void OnDisable()
     {
         PlayerHealth.OnPlayerDamaged -= RecieveDamage;
         PlayerHealth.OnPlayerHealthSetUp -= Initialize;
+        PlayerHealth.OnLifeIncreased -= HealDamage;
     }
     
     public void Initialize()
@@ -35,6 +38,7 @@ public class PlayerHealthUIView : MonoBehaviour
             _newPosition.x -= _heartsDistance;
             HealthHeartAnimation newHealthHeartPrefab = Instantiate(_healthHeartPrefab, Vector3.zero, _healthHeartPrefab.transform.rotation, transform);
             newHealthHeartPrefab.transform.localPosition = _newPosition;
+            newHealthHeartPrefab.SaveInitialState(_newPosition, transform.localPosition);
             _healthHearts.Add(newHealthHeartPrefab);
         }
     }
@@ -53,5 +57,10 @@ public class PlayerHealthUIView : MonoBehaviour
             _nearDeath = true;
         }
         _healthHearts[_playerHealth.Value].DoDamage();
+    }
+
+    private void HealDamage()
+    {
+        _healthHearts[_playerHealth.Value-1].DoHeal(_bonusTextTransform.localPosition);
     }
 }
